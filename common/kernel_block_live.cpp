a34c404ec45a09dfdc0cc3cfd182ed79179df74d
David Riusech <v1X3Q0@gmail.com>
2022-06-14 19:44:58 -0400

    needed to change min and max to local min and max definitions for os specific ops

--- common/kernel_block_live.cpp
+++ common/kernel_block_live.cpp
@@ -116,8 +116,8 @@ int kernel_block::consolidate_kmap_allocation(size_t kva, size_t kb_size, real_k
     }
     else
     {
-        new_kbase = std::min<size_t>(kva, collision_kmaps.front()->kva);
-        new_alloc_sz = max(kva + kb_size, collision_kmaps.back()->kva + collision_kmaps.back()->alloc_size) -
+        new_kbase = LOCAL_MIN<size_t>(kva, collision_kmaps.front()->kva);
+        new_alloc_sz = LOCAL_MAX(kva + kb_size, collision_kmaps.back()->kva + collision_kmaps.back()->alloc_size) -
             std::min<size_t>(kva, collision_kmaps.front()->kva);
     }
     new_alloc = (char*)calloc(new_alloc_sz, 1);
